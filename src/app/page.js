52508c8cb0090dfa81888e7ad621a3a5aae2b08a
SyawalAjg <mussyawal12@gmail.com>
2024-09-23 20:25:27 +0530

    update

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <>
+        <div className="">
+          <h1 className="mt-24 text-6xl font-garamond md:Apple Color Emoji">P,</h1>
+          <h1 className=" text-6xl font-garamond md:Apple Color Emoji text-blue-600/100"> I'am Mussyawal</h1>
+          <h1 className=" text-6xl font-garamond md:Apple Color Emoji">Cyber Security And UI/UX Design</h1>
+          <h1 className=" mt-10 text-3xl font-garamond md:Apple Color Emoji"> Saya kuliah di Politeknik LP3I Jakarta Prodi Manajemen Informatika</h1>
+          <div>
+            <button className=" mt-10 rounded-lg bg-yellow-500 w-40 h-10 min-h13 text-white">Get Started</button>
+          </div>
+        </div>           
+    </>
+  );
+}
