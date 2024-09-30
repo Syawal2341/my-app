db3de7ea25474ca386391e6de06b40ba53ea00bc
SyawalAjg <mussyawal12@gmail.com>
2024-09-30 17:34:41 +0530

    update

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -6,7 +6,7 @@ export default function Home() {
           <div className="rounded-xl">
           <h1 className="mt-24 text-6xl font-garamond md:Apple Color Emoji">P,</h1>
           <h1 className=" text-6xl font-garamond md:Apple Color Emoji text-blue-600/100"> I am Mussyawal</h1>
-          <h1 className=" text-6xl font-garamond md:Apple Color Emoji">Cyber Security And UI/UX Design</h1>
+          <h1 className=" text-6xl font-garamond md:Apple Color Emoji"> Cyber Security And UI/UX Design</h1>
           <h1 className=" mt-10 text-3xl font-garamond md:Apple Color Emoji"> Saya kuliah di Politeknik LP3I Jakarta Prodi Manajemen Informatika</h1>
           <div>
             <button className=" mt-10 rounded-lg bg-yellow-500 w-40 h-10 min-h13 text-white">Get Started</button>
