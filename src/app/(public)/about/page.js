77f91e401b33afc46d23fb6f5f7f437de47f7274
SyawalAjg <mussyawal12@gmail.com>
2024-10-28 20:19:33 +0530

    up

--- src/app/(public)/about/page.js
+++ src/app/(public)/about/page.js
@@ -5,10 +5,7 @@ export default function About() {
               About Mussyawal Baihaki
           </h1>
           <p className="text-lg text-gray-600 text-center max-w-2xl mb-6">
-              Mussyawal Baihaki is a premier destination for outdoor enthusiasts, 
-              offering breathtaking views and a range of activities from hiking 
-              to rock climbing. Our mission is to provide a safe and enjoyable 
-              experience for all adventurers.
+              Mussyawal Baihaki lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum.
           </p>
           <div className="flex flex-wrap justify-center">
               <button className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-200">
