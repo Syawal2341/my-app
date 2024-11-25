68c33c9895d59d96830abe63535e063ad52d89c6
SyawalAjg <mussyawal12@gmail.com>
2024-11-25 21:01:47 +0530

    up

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -2,15 +2,20 @@ export default function Home() {
   return (
     <>
       <div className="">
-        <h1 className="mt-24 text-6xl font-sans md:Apple Color Emoji">Hallo, </h1>
-        <h1 className=" text-6xl font-sans md:Apple Color Emoji text-yellow-600/100 " > Saya Mussyawal Baihaki </h1>
-        <p className=" text-6xl font-sans md:Apple Color Emoji" >Cyber Security And UI/UX Design</p>
+        <h1 className="mt-24 text-6xl font-sans md:Apple Color Emoji">
+          Hallo,{" "}
+        </h1>
+        <h1 className=" text-6xl font-sans md:Apple Color Emoji text-yellow-600/100 ">
+          {" "}
+          Saya Mussyawal Baihaki{" "}
+        </h1>
+        <p className=" text-6xl font-sans md:Apple Color Emoji">
+          Cyber Security And UI/UX Design
+        </p>
         <div>
-          <button className=" mt-20 rounded-lg bg-red-500 w-48 min-h-9 text-white" >Get Started</button>
+          <button href="/about" className="mt-20 rounded-lg bg-red-500 w-48 min-h-9 text-white inline-block text-center btn-link">Get Started</button>
         </div>
       </div>
     </>
-    
-    
   );
 }
