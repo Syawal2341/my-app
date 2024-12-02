eaf09190f7cc8db8bece991a73329335d3957829
SyawalAjg <mussyawal12@gmail.com>
2024-12-02 19:51:01 +0530

    up

--- src/app/(public)/about/page.js
+++ src/app/(public)/about/page.js
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
+      <div className="flex flex-col items-center justify-center min-h-screen bg-green-200 p-6">
           <h1 className="text-4xl font-bold text-gray-800 mb-4">
               About Mussyawal Baihaki
           </h1>
---
+++ src/app/(public)/blogs/page.js
@@ -0,0 +1,20 @@
+export default function Blogs(){
+    return (
+        <>
+
+        <h2 className="text-center text-[32px] font-bold w-full">Blogs</h2>
+        <p className="text-center margin-0 mx-auto w-2/3">
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        </p>
+
+        <div class="grid grid-cols-4 gap-4">
+            <div>
+                <div>
+                    <img src=""></img>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
