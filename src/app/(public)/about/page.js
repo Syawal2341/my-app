69b22139f2c8ecb92a26bc4c2665a1de158c89e7
Syawal <mussyawal12@gmail.com>
2024-12-14 16:28:08 +0700

    Public

--- src/app/(public)/about/page.js
+++ src/app/(public)/about/page.js
@@ -1,39 +1,7 @@
 export default function About() {
-<<<<<<< HEAD
-    return (
-      <div>
-        tes about
-      </div>
-    );
-}
-  
-=======
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200 p-6">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">
-        Mussyawal Baihaki
-      </h1>
-      <p className="text-lg text-gray-600 text-center max-w-2xl mb-6">
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum.
-      </p>
-      <div className="flex flex-wrap justify-center">
-        <div>
-        <a href="/">        
-          <button className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-200">
-            Learn More
-          </button>
-        </a>
-        </div>
-        <div>
-        <a href="/contact"> 
-          <button className="ml-4 bg-gray-300 text-gray-800 font-semibold py-2 px-4 rounded-lg hover:bg-gray-400 transition duration-200">
-            Contact Us
-          </button>
-        </a>
-        </div>
-      </div>
+    <div>
+      tes about
     </div>
   );
 }
->>>>>>> 6eac28cdab5ec8534e4917d557b5211bdad0c76d
