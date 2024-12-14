06a219d396373ba9f2922f62ebeadadd7ab3b97b
Syawal <mussyawal12@gmail.com>
2024-12-14 16:19:12 +0700

    up

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -1,16 +1,72 @@
-export default function Home() {
-  return (
-    <>
-      <div className="">
-        <h1 className="mt-24 text-6xl font-sans md:Apple Color Emoji">Hallo, </h1>
-        <h1 className=" text-6xl font-sans md:Apple Color Emoji text-yellow-600/100 " > Saya Mussyawal Baihaki </h1>
-        <p className=" text-6xl font-sans md:Apple Color Emoji" >Cyber Security And UI/UX Design</p>
-        <div>
-          <button className=" mt-20 rounded-lg bg-red-500 w-48 min-h-9 text-white" >Get Started</button>
-        </div>
+
+const ItemService=({
+  title,
+  subTitle
+})=>{
+
+  return (<>
+    <div className="w-[310px] bg-white p-5 ">
+      <div className="flex justify-center">
+        <img src="images/coding.png"/>
       </div>
-    </>
-    
-    
-  );
+      <div className="text-center">
+        <h3>{title}</h3>
+        <span className="text-[15px] text-[#767676]">{subTitle}</span>
+      </div>
+    </div>
+  </>)
 }
+
+export default function Dashboard() {
+  const myService = [
+    {title:'web development', subTitle:'blog, e-commerce'},
+    {title:'uI/uX design', subTitle:'Mobile app, website design'},
+    {title:'sound design', subTitle:'Voice Over, Beat Making'},
+    {title:'game design', subTitle:'Character Design, Props & Objects'},
+    {title:'photography', subTitle:'portrait, product photography'},
+    {title:'advertising', subTitle:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+  ]
+
+    return (
+      <>
+        <div className='flex w-full bg-white rounded-xl '>
+          <div className="flex-1 p-10">
+            <div className="font-extrabold text-[48px] mt-10">
+              <div>I&lsquo;m Rayan Adlrdard</div>
+              <div>
+                <span className="text-[#FFB400]">Front-end</span> Developer 
+              </div>
+            </div>
+            <p className="text-[#767676]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, volutpat feugiat placerat lobortis. Natoque rutrum semper sed suspendisse nunc lectus.</p>
+            <button className="btn-primary mt-5">
+              <span className="relative text-sm font-semibold text-white">
+                  Here Me
+              </span>
+            </button>
+          </div>
+          <div>
+            <img src="images/avatar.png" />
+          </div>
+        </div>
+
+        <div className="mt-20 text-center">
+          <h2 className="text-[32px] font-bold">My services</h2>
+          <div className="flex justify-center text-[#767676]">
+            <p className="w-1/2	">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum</p>
+          </div>
+        </div>
+
+        <div className="grid grid-rows-2 grid-flow-col gap-3 mt-10">
+          {
+            myService.
+              map((item,key)=><ItemService
+                                key={key} 
+                                title={item.title} 
+                                subTitle={item.subTitle} 
+                              />)
+          }
+        </div>
+      </>
+    );
+  }
+  
