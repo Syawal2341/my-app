6eac28cdab5ec8534e4917d557b5211bdad0c76d
Syawal <mussyawal12@gmail.com>
2024-12-14 13:50:47 +0700

    up

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -32,9 +32,11 @@ export default function Dashboard() {
         <div className='flex w-full bg-white rounded-xl '>
           <div className="flex-1 p-10">
             <div className="font-extrabold text-[48px] mt-10">
-              <div>I&lsquo;m Rayan Adlrdard</div>
+              <div>I&lsquo;m Mussyawal Baihaki</div>
               <div>
-                <span className="text-[#FFB400]">Front-end</span> Developer 
+                <span className="text-[#FFB400]">Fullstack Developer </span>
+                <>And </>
+                <span className="text-[#FFB400]">Cyber Security</span>
               </div>
             </div>
             <p className="text-[#767676]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, volutpat feugiat placerat lobortis. Natoque rutrum semper sed suspendisse nunc lectus.</p>
